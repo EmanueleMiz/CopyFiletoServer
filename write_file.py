96bf3b8dff23e1e5ed0330114ae32799a26d7a01
EmanueleMiz <83234372+EmanueleMiz@users.noreply.github.com>
2022-11-23 12:16:01 +0100

    Update write_file.py

--- write_file.py
+++ write_file.py
@@ -2,7 +2,7 @@ import tempfile
 from smb.SMBConnection import SMBConnection
 
 def copia():
-    connection = SMBConnection(username='admin',password='Serenissima1-',my_name='admin',remote_name='',domain='',use_ntlm_v2=True)
+    connection = SMBConnection(username='admin',password='',my_name='admin',remote_name='',domain='',use_ntlm_v2=True)
     connection.connect(ip='172.31.1.115')
     with open('./Prova.txt', 'rb', buffering=0) as file_obj:
     #file = open('./prova.txt', 'r')
